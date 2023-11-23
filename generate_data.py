6ae78f45587e27b6ced37eb09fa6889eb60ebe3e
Winnie Chow <wc459@cornell.edu>
2023-11-23 15:08:39 -0800

    Add data

---
+++ generate_data.py
@@ -0,0 +1,40 @@
+from datasets import load_dataset
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+
+dataset = load_dataset("EdinburghNLP/xsum", split="train")
+batch_size = 256
+
+# load in model for paraphrasing
+model = AutoModelForSeq2SeqLM.from_pretrained("Vamsi/T5_Paraphrase_Paws", torch_dtype=torch.bfloat16).to("cuda")
+tokenizer = AutoTokenizer.from_pretrained("Vamsi/T5_Paraphrase_Paws")
+
+# map summary of dataset to add prefix and suffix
+def format_dataset(example):
+    example["formatted"] = "paraphrase: " + example["summary"] + " </s>"
+    return example
+
+def tokenize(example):
+    return tokenizer(example["formatted"], return_tensors="pt", padding=True)
+
+dataset = dataset.map(format_dataset, remove_columns=['document'])
+dataset = dataset.with_format("torch")
+dataset = dataset.map(tokenize, batched=True, batch_size=batch_size)
+
+
+def paraphrase(example):
+    # print("---\n", example["input_ids"])
+    output = model.generate(
+        example["input_ids"].to("cuda"),
+        attention_mask=example["attention_mask"].to("cuda"),
+        max_length=256,
+        do_sample=True,
+        top_k=120,
+        top_p=0.95,
+        num_return_sequences=1
+    )
+    return {"generated": tokenizer.batch_decode(output, skip_special_tokens=True,clean_up_tokenization_spaces=True)}
+
+dataset = dataset.map(paraphrase, remove_columns=["input_ids", "attention_mask", "formatted", "id"], batched=True, batch_size=batch_size)
+
+dataset.save_to_disk("/jagupard25/scr0/wychow/EvadingDetectGPT/data")
---
+++ generate_data.sh
@@ -0,0 +1,2 @@
+export TRANSFORMERS_CACHE=/jagupard25/scr0/wychow/.cache/huggingface
+python generate_data.py
