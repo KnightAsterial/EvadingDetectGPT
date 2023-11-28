eb2023c081ed4b7e052babab3757b5f1a7404eda
Ryan Zhao <ryanzhao@berkeley.edu>
2023-11-27 19:59:09 -0800

    Code for generating t5-sentencewise-paraphrasing on wiki dataset

--- generate_data.py
+++ generate_data.py
@@ -4,7 +4,7 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 import nltk
 nltk.download('punkt')
 
-dataset = load_dataset("EdinburghNLP/xsum", split="train")
+dataset = load_dataset("aadityaubhat/GPT-wiki-intro", split="train")
 batch_size = 32
 
 # load in model for paraphrasing
@@ -12,13 +12,13 @@ model = AutoModelForSeq2SeqLM.from_pretrained("Vamsi/T5_Paraphrase_Paws", torch_
 tokenizer = AutoTokenizer.from_pretrained("Vamsi/T5_Paraphrase_Paws")
 
 # map summary of dataset to add prefix and suffix
-def format_dataset(example):
-    example["formatted"] = "paraphrase: " + example["document"] + " </s>"
-    # example["formatted"] = "paraphrase: " + example["summary"] + " </s>"
-    return example
+# def format_dataset(example):
+#     example["formatted"] = "paraphrase: " + example["document"] + " </s>"
+#     # example["formatted"] = "paraphrase: " + example["summary"] + " </s>"
+#     return example
 
-def tokenize(example):
-    return tokenizer(example["formatted"], return_tensors="pt", padding=True)
+# def tokenize(example):
+#     return tokenizer(example["formatted"], return_tensors="pt", padding=True)
 
 # dataset = dataset.filter(lambda example: len(tokenizer(example["document"])["input_ids"]) < 254)
 # dataset = dataset.map(format_dataset, remove_columns=['summary'])
@@ -27,54 +27,76 @@ def tokenize(example):
 # dataset = dataset.map(format_dataset, remove_columns=['document'])
 
 #---------
-def process(example):
-    lines = example['document'].split('\n')
-    new_lines = []
-    for line in lines:
-        line = line.strip()
-        if len(line) == 0:
-            new_lines.append(line)
-        else:
-            sents = nltk.sent_tokenize(line)
+# def process(example):
+#     lines = example['document'].split('\n')
+#     new_lines = []
+#     for line in lines:
+#         line = line.strip()
+#         if len(line) == 0:
+#             new_lines.append(line)
+#         else:
+#             sents = nltk.sent_tokenize(line)
 
-            formatted_sents = ["paraphrase: " + sent + " </s>" for sent in sents]
-            token_dict = tokenizer(formatted_sents, padding=True, return_tensors="pt")
-            output = model.generate(
-                token_dict["input_ids"].to("cuda"),
-                attention_mask=token_dict["attention_mask"].to("cuda"),
-                max_length=256,
-                do_sample=True,
-                top_k=200,
-                top_p=0.95,
-                num_return_sequences=1
-            )
-            paraphrased_sents = tokenizer.batch_decode(output, skip_special_tokens=True,clean_up_tokenization_spaces=True)
-            paraphrased_sents = [paraphrased_sent.strip() for paraphrased_sent in paraphrased_sents]
-            new_lines.append(' '.join(paraphrased_sents))
-            #new_lines.append(paraphrased_sents[0])
-    return {"generated": '\n'.join(new_lines)}
+#             formatted_sents = ["paraphrase: " + sent + " </s>" for sent in sents]
+#             token_dict = tokenizer(formatted_sents, padding=True, return_tensors="pt")
+#             output = model.generate(
+#                 token_dict["input_ids"].to("cuda"),
+#                 attention_mask=token_dict["attention_mask"].to("cuda"),
+#                 max_length=256,
+#                 do_sample=True,
+#                 top_k=200,
+#                 top_p=0.95,
+#                 num_return_sequences=1
+#             )
+#             paraphrased_sents = tokenizer.batch_decode(output, skip_special_tokens=True,clean_up_tokenization_spaces=True)
+#             paraphrased_sents = [paraphrased_sent.strip() for paraphrased_sent in paraphrased_sents]
+#             new_lines.append(' '.join(paraphrased_sents))
+#             #new_lines.append(paraphrased_sents[0])
+#     return {"generated": '\n'.join(new_lines)}
 
-dataset = dataset.filter(lambda example: len(tokenizer(example["document"])["input_ids"]) < 510)
-dataset = dataset.map(process, remove_columns=['summary', 'id'])
+# dataset = dataset.filter(lambda example: len(tokenizer(example["document"])["input_ids"]) < 510)
+# dataset = dataset.map(process, remove_columns=['summary', 'id'])
 #--------
 
+def split_entry(examples):
+    outputs = []
+    for document in examples["wiki_intro"]:
+        paragraphs = document.split('\n')
+        for para in paragraphs:
+            sents = nltk.sent_tokenize(para)
+            outputs.extend(sents)
+    return {'human_sents': outputs}
+
+def format_sentences(example):
+    example["formatted"] = "paraphrase: " + example["human_sents"] + " </s>"
+    # example["formatted"] = "paraphrase: " + example["summary"] + " </s>"
+    return example
+
+def tokenize(example):
+    return tokenizer(example["formatted"], return_tensors="pt", padding=True)
+            
+
+dataset = dataset.filter(lambda example: len(tokenizer(example["document"])["input_ids"]) < 510)
+dataset = dataset.map(split_entry, batched=True, remove_columns=dataset.column_names, num_proc=8)
+dataset = dataset.map(format_sentences, num_proc=8)
+
 dataset = dataset.with_format("torch")
-# dataset = dataset.map(tokenize, batched=True, batch_size=batch_size)
+dataset = dataset.map(tokenize, batched=True, batch_size=batch_size)
 
 
-# def paraphrase(example):
-#     # print("---\n", example["input_ids"])
-#     output = model.generate(
-#         example["input_ids"].to("cuda"),
-#         attention_mask=example["attention_mask"].to("cuda"),
-#         max_length=256,
-#         do_sample=True,
-#         top_k=200,
-#         top_p=0.95,
-#         num_return_sequences=1
-#     )
-#     return {"generated": tokenizer.batch_decode(output, skip_special_tokens=True,clean_up_tokenization_spaces=True)}
+def paraphrase(example):
+    # print("---\n", example["input_ids"])
+    output = model.generate(
+        example["input_ids"].to("cuda"),
+        attention_mask=example["attention_mask"].to("cuda"),
+        max_length=256,
+        do_sample=True,
+        top_k=200,
+        top_p=0.95,
+        num_return_sequences=1
+    )
+    return {"ai_sents": tokenizer.batch_decode(output, skip_special_tokens=True,clean_up_tokenization_spaces=True)}
 
-# dataset = dataset.map(paraphrase, remove_columns=["input_ids", "attention_mask", "formatted", "id"], batched=True, batch_size=batch_size)
+dataset = dataset.map(paraphrase, remove_columns=["input_ids", "attention_mask", "formatted"], batched=True, batch_size=batch_size)
 print(dataset[:2])
-dataset.save_to_disk("/scratch/users/ryanzhao/EvadingDetectGPT/data")
+dataset.save_to_disk("/scratch/users/ryanzhao/EvadingDetectGPT/data_t5_wikidoc")
