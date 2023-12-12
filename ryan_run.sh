5cfbbe7eba556dd9bb403ddb1875ad90994292a8
Ryan Zhao <ryanzhao@sh02-12n11.int>
2023-12-11 17:10:32 -0800

    Updated ryan_run.sh

--- ryan_run.sh
+++ ryan_run.sh
@@ -4,8 +4,16 @@ export HF_DATASETS_CACHE=$SCRATCH/.cache/huggingface/datasets
 
 # python maml.py --device gpu --batch_size 1 --num_support 4 --checkpoint_step 60000 --log_dir "/scratch/users/ryanzhao/EvadingDetectGPT/logs/maml/evadegpt.support_4.query_1.inner_steps_1.inner_lr_0.4.learn_inner_lrs_False.outer_lr_0.001.batch_size_2.iters_300000" --test --test_output_dir "/scratch/users/ryanzhao/EvadingDetectGPT/modeltest_cp6k"
 # python maml.py --device gpu --batch_size 1 --num_support 4 --test --test_output_dir "/scratch/users/ryanzhao/EvadingDetectGPT/modeltest_cp0"
-python maml.py --device gpu --batch_size 1 --num_support 4 --test --test_output_dir "/scratch/users/ryanzhao/EvadingDetectGPT/modeltest_cp0_noinnerloop" --test_skip_innerloop
+# python maml.py --device gpu --batch_size 1 --num_support 4 --test --test_output_dir "/scratch/users/ryanzhao/EvadingDetectGPT/modeltest_cp0_noinnerloop" --test_skip_innerloop
+# python maml.py --device gpu --batch_size 1 --num_support 4 --test --test_output_dir "/scratch/users/ryanzhao/EvadingDetectGPT/modeltest_humanai"
 
 # python maml.py --device gpu --batch_size 2 --num_support 4 --num_train_iterations 300000 --checkpoint_step 30000
 # python main.py
-# python evaluate.py
+# python evaluate.py --method "modeltest_cp6k" --ai_label "ai_sample" --human_label "rephrased_sample" --dataset_dir "./modeltest_cp6k"
+# python evaluate.py --method "modeltest_cp0_nil" --ai_label "ai_sample" --human_label "rephrased_sample" --dataset_dir "./modeltest_cp0_noinnerloop"
+# python evaluate.py --method "modeltest_cp0_wil" --ai_label "ai_sample" --human_label "rephrased_sample" --dataset_dir "./modeltest_cp0_withinnerstep"
+
+# python multitask_learning.py --device gpu --batch_size 1 --num_support 10 --num_query 1 --max_num_edits 11 --checkpoint_step 27000 --num_train_iterations 2000000
+python multitask_learning.py --device gpu --batch_size 1 --num_support 10 --num_query 1 --max_num_edits 11 --outer_lr 0.01 --num_train_iterations 5000
+# python multitask_learning.py --device gpu --batch_size 1 --num_support 4 --num_query 1 --max_num_edits 11 --checkpoint_step 27000 --log_dir "/scratch/users/ryanzhao/EvadingDetectGPT/logs/multitask/evadegpt.support_10.query_1.outer_lr_0.001.batch_size_1.iters_2000000" --test --test_output_dir "/scratch/users/ryanzhao/EvadingDetectGPT/multitasktest_cp27k_f32"
+# python multitask_learning.py --device gpu --batch_size 1 --num_support 4 --num_query 1 --max_num_edits 11 --checkpoint_step 594000 --log_dir "/scratch/users/ryanzhao/EvadingDetectGPT/logs/multitask/evadegpt.support_10.query_1.outer_lr_0.001.batch_size_1.iters_3000000" --test --test_output_dir "/scratch/users/ryanzhao/EvadingDetectGPT/multitasktest_cp594k"
